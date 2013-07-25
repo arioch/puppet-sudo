50cc58add2f3a642a3b5e3b79759f952105edc29
Tom De Vylder <tom@penumbra.be>
2013-07-25 14:17:00 +0200

    Rearrange spec layout

--- Rakefile
+++ Rakefile
@@ -1,10 +1,17 @@
 require 'rake'
-require 'puppet-lint/tasks/puppet-lint'
 require 'rspec/core/rake_task'
+require 'puppetlabs_spec_helper/rake_tasks'
 
-RSpec::Core::RakeTask.new(:rspec) do |t|
+RSpec::Core::RakeTask.new(:spec_verbose) do |t|
   t.pattern = 'spec/*/*_spec.rb'
-  #t.rspec_opts = "--format documentation --color"
+  t.rspec_opts = File.read('spec/spec.opts').chomp || ""
 end
 
-task :default => :rspec
+task :test do
+  Rake::Task[:spec_prep].invoke
+  Rake::Task[:spec_verbose].invoke
+  Rake::Task[:spec_clean].invoke
+end
+
+
+task :default => [:spec, :lint]
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'rspec-puppet'
 
 fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
@@ -5,6 +6,18 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
-  c.formatter = 'documentation'
-  c.color = 'true'
+end
+
+def centos_facts
+  {
+    :operatingsystem => 'CentOS',
+    :osfamily        => 'RedHat',
+  }
+end
+
+def debian_facts
+  {
+    :operatingsystem => 'Debian',
+    :osfamily        => 'Debian',
+  }
 end
